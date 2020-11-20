a7ff887f563eeb012dd802c77f80ea52b91a53a2
njericooper <njericooper@gmail.com>
2020-11-20 15:40:36 -0600

    updated footer text

--- public/scripts/date.js
+++ public/scripts/date.js
@@ -3,6 +3,6 @@ window.addEventListener('load', setDate, false);
 function setDate() { 
     var date = new Date();
     var theYear = date.getFullYear();
-    var theText = window.document.getElementById("footerText").innerHTML =  " &copy; " + " Source & Stardust Holdings LLC " + theYear;
+    var theText = window.document.getElementById("footerText").innerHTML =  " &copy; " + " Source & Stardust " + theYear;
     return theText;
 }
